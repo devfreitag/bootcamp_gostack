6377dad45e388178388f7063c36728f795116496
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-04-13 00:42:13 -0300

    configure file and css loader to use in webpack

--- frontendWithReactJS/src/App.js
+++ frontendWithReactJS/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import './App.css';
+import backgroundImage from './assets/background.jpg'
+
 import Header from './components/Header';
 
 function App() {
@@ -12,11 +15,14 @@ function App() {
   return (
     <>
       <Header title="Projects"/>
+
+      <img src={backgroundImage} alt="Background"/>
+
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
       </ul>
 
-      <button type="button" onClick={handleAddProject}>Adicionar projecto</button>
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
     </>
   )
 }

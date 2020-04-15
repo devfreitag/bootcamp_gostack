e96c308f6ead8a12e28701a8eaf7e6c125799831
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-04-15 01:07:50 -0300

    add backend integration and list projects

--- mobileWithReactNative/src/index.js
+++ mobileWithReactNative/src/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(res => {
+      setProjects(res.data);
+    })
+  }, []);
+  
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor='#7159c1'/>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item:project }) => (
+            <Text key={project.id} style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   );
 }
@@ -16,12 +32,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
   },
-  title: {
+  project: {
     fontSize: 32,
-    fontWeight: 'bold',
     color: '#FFF'
   }
 })

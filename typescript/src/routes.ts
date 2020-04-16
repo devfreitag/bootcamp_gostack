880f507bdfd7ad9d61ba9e3e3e78d93077759c52
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-04-15 21:09:30 -0300

    learn about interface and arrays with differents types inside

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function returnRes(req: Request, res: Response) {
-  return res.json({ ok: true });
+  const user = createUser({
+    email:'alex@email.com',
+    password: 'alex123',
+    techs: [
+      'ReactJS',
+      'NodeJS',
+      { title: 'Javascript', experience: 100 },
+    ]
+  });
+
+  return res.json(user);
 };

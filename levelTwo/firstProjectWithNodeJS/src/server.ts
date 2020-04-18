55439bb97d3f21176df279d7d5f276151451e050
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-04-17 23:22:48 -0300

    add eslint

--- levelTwo/firstProjectWithNodeJS/src/server.ts
+++ levelTwo/firstProjectWithNodeJS/src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ ok: true });
-})
+app.get('/', (req, res) => res.json({ ok: true }));
 
 app.listen(3333, () => {
-  console.log('Back-end started!')
-})
+  console.log('Back-end started!');
+});

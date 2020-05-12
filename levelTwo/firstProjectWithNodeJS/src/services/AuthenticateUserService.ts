bb500b81205f6ac060ab84ac970841968ce417dd
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-11 22:38:44 -0300

    add authentication with email and password

---
+++ levelTwo/firstProjectWithNodeJS/src/services/AuthenticateUserService.ts
@@ -0,0 +1,39 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface RequestDTO {
+  email: string;
+  password: string;
+}
+
+interface ResponseDTO {
+  user: User;
+}
+
+class AuthenticaUserService {
+  public async execute({ email, password }: RequestDTO): Promise<ResponseDTO> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    return {
+      user,
+    };
+  }
+}
+
+export default AuthenticaUserService;

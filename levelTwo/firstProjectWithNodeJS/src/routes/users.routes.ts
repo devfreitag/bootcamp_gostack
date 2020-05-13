4ba24be20f66483ffb1ee162e827f67d1c316f6f
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-12 23:09:43 -0300

    update avatar in database

--- levelTwo/firstProjectWithNodeJS/src/routes/users.routes.ts
+++ levelTwo/firstProjectWithNodeJS/src/routes/users.routes.ts
@@ -4,6 +4,7 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -35,7 +36,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (req, res) => {
-    return res.send();
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFileName: req.file.filename,
+      });
+
+      delete user.password;
+
+      return res.json(user);
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
   },
 );
 

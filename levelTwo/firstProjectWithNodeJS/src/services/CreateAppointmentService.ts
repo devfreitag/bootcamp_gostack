5be031ed1756a176782db4cf03451a2625d3a394
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-12 23:28:16 -0300

    create a AppError class to be used instead of Error default

--- levelTwo/firstProjectWithNodeJS/src/services/CreateAppointmentService.ts
+++ levelTwo/firstProjectWithNodeJS/src/services/CreateAppointmentService.ts
@@ -1,5 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
 import Appointment from '../models/Appointment';
 import AppointmentRepository from '../repositories/AppointmentsRepository';
 
@@ -22,7 +25,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointment is already booked');
+      throw new AppError('This appointment is already booked');
     }
 
     const appointment = appointmentsRepository.create({

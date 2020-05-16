488c5d0faa2e61d263c9349ddba2f08d53b62c2d
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-16 18:18:23 -0300

    make the dashboard layout

--- levelThree/react_project/src/pages/Dashboard/index.tsx
+++ levelThree/react_project/src/pages/Dashboard/index.tsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Dashboard</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/32180411?s=460&u=97739794ff3339ac806059027fd482709fb4cc3d&v=4"
+            alt="Alex Freitag"
+          />
+          <div>
+            <strong>alexxfreitag/bootcamp_gostack</strong>
+            <p>Descrição do repositório</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/32180411?s=460&u=97739794ff3339ac806059027fd482709fb4cc3d&v=4"
+            alt="Alex Freitag"
+          />
+          <div>
+            <strong>alexxfreitag/bootcamp_gostack</strong>
+            <p>Descrição do repositório</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/32180411?s=460&u=97739794ff3339ac806059027fd482709fb4cc3d&v=4"
+            alt="Alex Freitag"
+          />
+          <div>
+            <strong>alexxfreitag/bootcamp_gostack</strong>
+            <p>Descrição do repositório</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;

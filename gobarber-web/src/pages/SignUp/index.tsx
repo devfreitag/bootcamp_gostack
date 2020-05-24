faacea36ffd662dc3ad06586911bf2ee34d3d27a
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-05-24 11:07:46 -0300

    move errors to component Tooltip

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -30,7 +30,6 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
       const errors = getValidationErrors(err);
       formRef.current?.setErrors(errors);
     }

7c81631cea161cc481c3a6659ecfbea25f51ae60
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-07-01 20:28:49 -0300

    move code from repository 'desafio_conceitos_reactjs' to locally

---
+++ challenges/desafio_conceitos_reactjs/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(res => {
+      setRepositories(res.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    
+    const response = await api.post('repositories', {
+      title: "Be The Hero",
+      url: "https://github.com/alexxfreitag/be_the_hero",
+      techs: ["React Native", "ReactJS", "NodeJS", "Javascript"]
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter((repository) => repository.id !== id));
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+          <li key={repository.id}>{repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
